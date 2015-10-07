2cfab98675deec0a97f937af419244ecccc43920
Jermaine Kee <godson919@icloud.com>
2015-10-07 13:00:05 -0400

    update

--- movieratings/Rater/models.py
+++ movieratings/Rater/models.py
@@ -10,18 +10,17 @@ class Rater(models.Model):
         return 'Rater ID: {}'.format(self.rater_id)
 
 
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
 class Rating(models.Model):
     user = models.ForeignKey(Rater)
-    movie = models.ForeignKey(Movie)
+    title = models.ForeignKey(Movie)
     rating = models.PositiveSmallIntegerField()
 
     def __str__(self):
         return 'user {} gives {} a {}'.format(self.user, self.movie, self.rating)
-
-
-class Movie(models.Model):
-    title = models.CharField(max_length=255)
-
-
-    def __str__(self):
-        return '{}'.format(self.title)
