bc8a60139e85457f23df7c55beecdf86a2cc3ece
Jermaine Kee <godson919@icloud.com>
2015-10-07 13:24:17 -0400

    new models in moviedb

---
+++ movieratings/moviedb/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+# Create your models here.
+
+
+class Rater(models.Model):
+    rater_id = models.PositiveIntegerField()
+
+    def __str__(self):
+        return 'Rater ID: {}'.format(self.rater_id)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(Rater)
+    title = models.ForeignKey(Movie)
+    rating = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return 'user {} gives {} a {}'.format(self.user, self.movie, self.rating)
