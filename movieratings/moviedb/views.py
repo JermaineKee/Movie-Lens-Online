939239661f4c8ea7199a6e9af6d5c2dd10ee1d00
Jermaine Kee <godson919@icloud.com>
2015-10-09 13:59:53 -0400

    clean slate

--- movieratings/moviedb/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def movie_detail(request, movie_id):
-    movie = Movie.objects.get(pk=movie_id)
-    return render(request,
-                   '.html',
-                   {'movie': movie})
