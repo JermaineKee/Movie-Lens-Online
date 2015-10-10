90c399f332fe16b4e9a228a2040fb7c421fc29aa
Jermaine Kee <godson919@icloud.com>
2015-10-09 22:29:29 -0400

    models complete

--- movieratings/cinemaapp/admin.py
+++ movieratings/cinemaapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Movie, Rater, Rating
 
 # Register your models here.
+
+
+class RaterAdmin(admin.ModelAdmin):
+    list_display = ['id', 'gender', 'occupation', 'zipcode']
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'rater', 'movie', 'stars']
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title']
--- movieratings/cinemaapp/models.py
+++ movieratings/cinemaapp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Rater(models.Model):
+
+    MALE = 'M'
+    FEMALE = 'F'
+
+    GENDER_CHOICES = (
+        (MALE, 'M'),
+        (FEMALE, 'F'),
+    )
+
+    age = models.PositiveSmallIntegerField()
+    occupation = models.CharField(max_length=40)
+    gender = model.CharField(max_length=1, choices=GENDER_CHOICES)
+    zipcode = models.CharField(max_length=5)
+
+    def __str__(self):
+        return (self.id)
+
+
+class Movie(models.Model):
+    title = models.CharFields(max_length=255)
+
+    def average_rating(self):
+        return self.rating_set.aggregate(models.Avg('rating'))['rating__avg']
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
+
+class Rating(models.Model):
+    rater = models.ForeignKey(Rater)
+    movie = models.ForeignKey(Movie)
+    stars = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return 'Rater {}, Title: {}, Rating: {}'.format
+        (self.rater, self.movie, self.rating)
